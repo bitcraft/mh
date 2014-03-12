e886550889708fb5b0064ede87a3afd6387af8c7
bitcraft <leif.theden@gmail.com>
2014-03-11 19:32:03 -0500

    removed cleanup.sh

--- cleanup.sh
+++
@@ -1,7 +0,0 @@
-find . -name \.DS_Store -type f -delete
-find . -name \*~ -type f -delete
-find . -name \*pyc -type f -delete
-find . -name \*.swp -type f -delete
-find . -type d -print0 | xargs -0 chmod 755
-find . -type f -print0 | xargs -0 chmod 644
-find . -type f -print0 | xargs -0 touch -ma
